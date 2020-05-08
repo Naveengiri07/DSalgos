739e2486e77e74ed5e89f90668f510b17e9d35b5
Naveen Giri Goswami <46757257+Naveengiri07@users.noreply.github.com>
2020-05-08 16:47:21 +0530

    Add files via upload

---
+++ Recursion/SumOfGPSeries.java
@@ -0,0 +1,39 @@
+package dsalgopracticecodes;
+
+import java.util.Scanner;
+
+public class SumOfGPSeries {
+	
+	static int sum = 0;
+	
+	static int sum(int a, int r, int n) {
+		if(a==0) {
+			return 0;
+		}
+		
+		if(n==0) {
+			return -1;
+		}
+		sum = sum+a;
+		sum(a*r, r, n-1);
+		return sum;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the first element of GP series: ");
+		int a = s.nextInt();
+		System.out.print("Enter the commom ratio: ");
+		int r = s.nextInt();
+		System.out.print("Enter the number of elements: ");
+		int n = s.nextInt();
+		
+		System.out.println(sum(a,r,n));
+		
+		
+
+	}
+
+}
