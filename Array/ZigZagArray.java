3338855163dc968c137a6e4e8da21398ae7469ac
Naveen Giri Goswami <46757257+Naveengiri07@users.noreply.github.com>
2020-05-14 16:58:05 +0530

    Add files via upload

---
+++ Array/ZigZagArray.java
@@ -0,0 +1,38 @@
+package dsalgopracticecodes;
+
+public class ZigZagArray {
+	
+	static void arrange(int a[]) {
+		int i=0;
+		while(i<a.length-1) {
+			if(i%2==0) {
+				if(a[i]>a[i+1]) {
+					int temp = a[i];
+					a[i]=a[i+1];
+					a[i+1]=temp;
+				}
+			}
+		    
+			else{
+				if(a[i]<a[i+1]) {
+					int temp = a[i];
+					a[i]=a[i+1];
+					a[i+1]=temp;
+				
+				}
+			}
+		i++;
+		}
+	}
+
+	public static void main(String[] args) {
+		int array[] = {4,3,7,8,6,2,1};
+		arrange(array);
+		for(int i:array) {
+			System.out.print(i+" ");
+		}
+		
+
+	}
+
+}
